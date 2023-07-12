82fe4f59dccad24cdb730d3522494c3328d51f1d
Nischal Gautam <nischal1024@gmail.com>
2023-07-12 20:37:49 +0545

    calculate rsi

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import generate
 import buyingfunc
 
 
+AMOUNT_ALLOCATED = 50000
+
 def sortingFunction(indicator, window_length, number_of_items_to_sort, stockClass = []):
   if indicator=="RSI":
     stocks = []
@@ -15,29 +17,13 @@ def sortingFunction(indicator, window_length, number_of_items_to_sort, stockClas
     return aa[number_of_items_to_sort-1]
 
 
-
 SPY = generate.StockSymbol("SPY")
-
 PSQ = generate.StockSymbol("PSQ" )
-
 SHY = generate.StockSymbol("SHY")
-
 QQQ =generate.StockSymbol("QQQ")
-
-
-cp =SPY.get_closing_price()
-ccp=  SPY.get_current_price()
-df = SPY.get_data_frame()
-rsi = SPY.get_rsi(14)
-rsi_value = SPY.get_rsi_value(14)
-sma = SPY.get_sma(200)
-smaVal = SPY.get_sma_value(200)
-
 
 is_current_price_of_spy_above_200d_moving_average = SPY.get_closing_price() > SPY.get_sma_value(window_length=200)
 is_current_price_of_qqq_less_than_20d_moving_average_price_of_qqq = QQQ.get_closing_price() < QQQ.get_sma_value(window_length=20)
-
-
 
 def define_logic():
     if is_current_price_of_spy_above_200d_moving_average:
@@ -49,8 +35,6 @@ def define_logic():
             buyingfunc.buy(symbol= x.get_symbol(),qty=1)
         else:
           buyingfunc.buy(symbol="QQQ",qty=1)
-
-
 
 
 # logic execution take in factors such as time
